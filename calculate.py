31cd5beb5443f3a8ccee9670ae95b16310b6b9a8
valek09 <valentinsadovnikov9@gmail.com>
2024-11-13 19:25:22 +0300

    2

--- calculate.py
+++ calculate.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
             map(
                 int,
                 input(
-                    "Input figure sizes separated by space, 1 for circle and square\n"
+                    "Input figure sizes separated by space\n"
                 ).split(),
             )
         )
